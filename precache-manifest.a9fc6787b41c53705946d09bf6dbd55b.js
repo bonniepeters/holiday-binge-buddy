4724266796a3598be13b7800c0374d7d7ae38f45
Bonnie Peters <bonniepeters@Bonnies-MacBook-Pro.local>
2019-11-18 20:43:52 -0700

    Updates

---
+++ precache-manifest.a9fc6787b41c53705946d09bf6dbd55b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0583b49b7c3a3c19c54a26352a261e2",
+    "url": "/holiday-binge-buddy/index.html"
+  },
+  {
+    "revision": "24658d9410380bc0aca6",
+    "url": "/holiday-binge-buddy/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "b295751876ccf06af60b",
+    "url": "/holiday-binge-buddy/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "24658d9410380bc0aca6",
+    "url": "/holiday-binge-buddy/static/js/2.d3a42123.chunk.js"
+  },
+  {
+    "revision": "b295751876ccf06af60b",
+    "url": "/holiday-binge-buddy/static/js/main.5141a45d.chunk.js"
+  },
+  {
+    "revision": "c0fbb8c2e093f88b0667",
+    "url": "/holiday-binge-buddy/static/js/runtime-main.d721ea98.js"
+  }
+]);
