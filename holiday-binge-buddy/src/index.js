7965823cd03e72569d3e7fc0184587e29c5c3888
bonniepeters <bonnie.e.peters@gmail.com>
2019-11-19 09:08:30 -0700

    deployment 100

--- holiday-binge-buddy/src/index.js
+++ holiday-binge-buddy/src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 
 ReactDOM.render(
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router basename={"/holiday-binge-buddy/#/"}>
         <App />
     </Router>,
     document.getElementById('root')
