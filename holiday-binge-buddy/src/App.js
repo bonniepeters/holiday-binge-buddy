ebd2183173e112ee13ef394f2b2d8fc6d6346778
bonniepeters <bonnie.e.peters@gmail.com>
2019-11-19 08:53:36 -0700

    deployment

--- holiday-binge-buddy/src/App.js
+++ holiday-binge-buddy/src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Route, Switch } from 'react-router-dom';
-import Shows from './Components/Shows/Shows';
-import Show from './Components/Show/Show';
-import Episodes from './Components/Episodes/Episodes';
-import Episode from './Components/Episode/Episode';
-import Header from './Components/Header/Header';
-import NewShow from './Components/NewShow/NewShow';
+import React, { Component } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Switch } from "react-router-dom";
+import Shows from "./Components/Shows/Shows";
+import Show from "./Components/Show/Show";
+import Episodes from "./Components/Episodes/Episodes";
+import Episode from "./Components/Episode/Episode";
+import Header from "./Components/Header/Header";
+import NewShow from "./Components/NewShow/NewShow";
 
 // console.log(process.env.REACT_APP_OMDB_API_KEY)
 // const API_KEY = process.env.REACT_APP_OMDB_API_KEY;
@@ -19,30 +19,25 @@ class App extends Component {
         <Header />
         <main>
           <Switch>
-            <Route exact={true} path="/" component={Shows} />            <Route exact={true} path="/show/add" component={NewShow} />
-            <Route exact={true} path="/episodes" component={Episodes} />
+            <Route exact={true} path="/" component={Shows} />{" "}
+            <Route exact={true} path="/holiday-binge-buddy/#/show/add" component={NewShow} />
+            <Route exact={true} path="/holiday-binge-buddy/#/episodes" component={Episodes} />
             <Route
-							path="/shows/:id"
-							render={routerProps => (
-								<Show
-									match={routerProps.match}
-									id={this.props.id}
-								/>
-							)}
+              path="/holiday-binge-buddy/#/shows/:id"
+              render={routerProps => (
+                <Show match={routerProps.match} id={this.props.id} />
+              )}
             />
             <Route
-							path="/episodes/:id"
-							render={routerProps => (
-								<Episode
-									match={routerProps.match}
-									id={this.props.id}
-								/>
-							)}
+              path="/holiday-binge-buddy/#/episodes/:id"
+              render={routerProps => (
+                <Episode match={routerProps.match} id={this.props.id} />
+              )}
             />
           </Switch>
         </main>
       </div>
-    )
+    );
   }
 }
 
